b926d8340f63ef275ecdfe3c08636e9071f3d1f6
Hermas Francisco <franciscohermas@gmail.com>
2026-02-13 15:26:30 +0100

    Configure MongoDB database connection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // dd(DB::connection('mongodb')->getCollection('listingsAndReviews')->countDocuments());
     return view('welcome');
 });
